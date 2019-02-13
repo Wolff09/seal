40edd7a266b3d3c7361c83dac745e527e1097354
Sebastian Wolff <s.wolff@cs.uni-kl.de>
2019-02-13 17:32:07 +0100

    tested and fixed simulation computation

--- src/types/check.cpp
+++ src/types/check.cpp
@@ -28,13 +28,76 @@ void print_guarantees(const GuaranteeSet set) {
 	std::cout << "}" << std::endl;
 }
 
-std::unique_ptr<Transition> mk_transition(const State& src, const State& dst, const Function& func, const ObserverVariable& obsvar) {
+std::unique_ptr<Transition> mk_transition_any_thread(const State& src, const State& dst, const Function& func, const ObserverVariable& obsvar) {
 	auto guard = std::make_unique<EqGuard>(obsvar, std::make_unique<ArgumentGuardVariable>(*func.args.at(0)));
 	return std::make_unique<Transition>(src, dst, func, Transition::INVOCATION, std::move(guard));
 }
 
+std::unique_ptr<Transition> mk_transition_invocation_self(const State& src, const State& dst, const Function& func, const ObserverVariable& obsvar_thread, const ObserverVariable& obsvar_arg) {
+	auto guard_thr = std::make_unique<EqGuard>(obsvar_thread, std::make_unique<SelfGuardVariable>());
+	auto guard_arg = std::make_unique<EqGuard>(obsvar_arg, std::make_unique<ArgumentGuardVariable>(*func.args.at(0)));
+	auto guard = std::make_unique<ConjunctionGuard>(std::move(guard_thr), std::move(guard_arg));
+	return std::make_unique<Transition>(src, dst, func, Transition::INVOCATION, std::move(guard));
+}
+
+std::unique_ptr<Transition> mk_transition_invocation_self_neg(const State& src, const State& dst, const Function& func, const ObserverVariable& obsvar_thread, const ObserverVariable& obsvar_arg) {
+	auto guard_thr = std::make_unique<EqGuard>(obsvar_thread, std::make_unique<SelfGuardVariable>());
+	auto guard_arg = std::make_unique<NeqGuard>(obsvar_arg, std::make_unique<ArgumentGuardVariable>(*func.args.at(0)));
+	auto guard = std::make_unique<ConjunctionGuard>(std::move(guard_thr), std::move(guard_arg));
+	return std::make_unique<Transition>(src, dst, func, Transition::INVOCATION, std::move(guard));
+}
+
+std::unique_ptr<Transition> mk_transition_response_self(const State& src, const State& dst, const Function& func, const ObserverVariable& obsvar_thread) {
+	auto guard = std::make_unique<EqGuard>(obsvar_thread, std::make_unique<SelfGuardVariable>());
+	return std::make_unique<Transition>(src, dst, func, Transition::RESPONSE, std::move(guard));
+}
+
 void prtypes::test() {
-	throw std::logic_error("not implemented");
+	// throw std::logic_error("not implemented");
+	
+	std::cout << std::endl << std::endl << "Testing..." << std::endl;
+	const Type& ptrtype = Observer::free_function().args.at(0)->type;
+
+	Program program;
+	program.name = "TestProgram";
+
+	// create retire function
+	program.functions.push_back(std::make_unique<Function>(" retire", Type::void_type(), Function::SMR));
+	Function& retire = *program.functions.back();
+	retire.args.push_back(std::make_unique<VariableDeclaration>("ptr", ptrtype, false));
+
+	// create protect function
+	program.functions.push_back(std::make_unique<Function>("protect", Type::void_type(), Function::SMR));
+	Function& protect = *program.functions.back();
+	protect.args.push_back(std::make_unique<VariableDeclaration>("ptr", ptrtype, false));
+
+	// hp observer
+	auto obs_hp_ptr = std::make_unique<Observer>();
+	auto& obs_hp = *obs_hp_ptr;
+	obs_hp.negative_specification = true;
+	obs_hp.variables.push_back(std::make_unique<ThreadObserverVariable>("T"));
+	obs_hp.variables.push_back(std::make_unique<ProgramObserverVariable>(std::make_unique<VariableDeclaration>("P", ptrtype, false)));
+
+	obs_hp.states.push_back(std::make_unique<State>("HP.0", true, false));
+	obs_hp.states.push_back(std::make_unique<State>("HP.1", false, false));
+	obs_hp.states.push_back(std::make_unique<State>("HP.2", false, false));
+	obs_hp.states.push_back(std::make_unique<State>("HP.3", false, false));
+	obs_hp.states.push_back(std::make_unique<State>("HP.4", false, true));
+
+	obs_hp.transitions.push_back(mk_transition_invocation_self(*obs_hp.states.at(0), *obs_hp.states.at(1), protect, *obs_hp.variables.at(0), *obs_hp.variables.at(1)));
+	obs_hp.transitions.push_back(mk_transition_response_self(*obs_hp.states.at(1), *obs_hp.states.at(2), protect, *obs_hp.variables.at(0)));
+	obs_hp.transitions.push_back(mk_transition_any_thread(*obs_hp.states.at(2), *obs_hp.states.at(3), retire, *obs_hp.variables.at(1)));
+	obs_hp.transitions.push_back(mk_transition_any_thread(*obs_hp.states.at(3), *obs_hp.states.at(4), Observer::free_function(), *obs_hp.variables.at(1)));
+
+	obs_hp.transitions.push_back(mk_transition_invocation_self_neg(*obs_hp.states.at(1), *obs_hp.states.at(0), protect, *obs_hp.variables.at(0), *obs_hp.variables.at(1)));
+	obs_hp.transitions.push_back(mk_transition_invocation_self_neg(*obs_hp.states.at(2), *obs_hp.states.at(0), protect, *obs_hp.variables.at(0), *obs_hp.variables.at(1)));
+	obs_hp.transitions.push_back(mk_transition_invocation_self_neg(*obs_hp.states.at(3), *obs_hp.states.at(0), protect, *obs_hp.variables.at(0), *obs_hp.variables.at(1)));
+
+	std::cout << std::endl << "Computing simulation... " << std::flush;
+	SimulationEngine engine;
+	engine.compute_simulation(obs_hp);
+	std::cout << "done" << std::endl;
+
 
 	// std::cout << std::endl << std::endl << "Testing..." << std::endl;
 	// const Type& ptrtype = Observer::free_function().args.at(0)->type;
--- src/types/simulation.cpp
+++ src/types/simulation.cpp
@@ -162,39 +162,60 @@ using namespace prtypes;
 
 
 
+struct TranslationUnit {
+	std::set<std::string> names;
+	std::vector<std::string> assertions;
 
+	template<typename... Targs>
+	static TranslationUnit merge(const Targs&... units) {
+		TranslationUnit result;
+		for (const auto& unit : { units... }) {
+			result.names.insert(unit.names.begin(), unit.names.end());
+			std::copy(unit.assertions.begin(), unit.assertions.end(), std::back_inserter(result.assertions));
+		}
+		return result;
+	}
 
+	std::string to_string() const {
+		static const std::string BREAK = "\n";
+		// static const std::string BREAK = " ";
+		std::string result;
+		for (auto& name : this->names) {
+			result += "(declare-const " + name + " Int)" + BREAK;
+		}
+		result += BREAK;
+		for (auto& assertion : this->assertions) {
+			result += "(assert " + assertion + ")" + BREAK;
+		}
+		result += BREAK + "(check-sat)";
+		return result;
+	}
+};
 
 struct TranslationVisitorWithPrefix : public ObserverVisitor {
 	const std::string prefix;
+	TranslationUnit translation;
 	std::set<std::string> names;
 	std::string result = "";
 
 	TranslationVisitorWithPrefix(std::string prefix) : prefix(prefix) {}
 
-	std::string get_decls() {
-		std::string result = "";
-		for (auto name : names) {
-			result = result + "(declare-const " + name + " Int) ";
-		}
-		return result;
-	}
-
-	std::pair<std::string, std::string> get_result() {
-		return { get_decls(), result };
+	TranslationUnit get_result() {
+		translation.assertions.push_back(result);
+		return translation;
 	}
 
 	void visit(const ThreadObserverVariable& var) {
-		result = "$" + var.name;
-		names.insert(result);
+		result = "oThr__" + var.name;
+		translation.names.insert(result);
 	}
 	void visit(const ProgramObserverVariable& var) {
-		result = "&" + var.decl->name;
-		names.insert(result);
+		result = "oPtr__" + var.decl->name;
+		translation.names.insert(result);
 	}
 	void visit(const SelfGuardVariable& /*var*/) {
-		result = "@self";
-		names.insert(result);
+		result = "__self__";
+		translation.names.insert(result);
 	}
 	void visit(const ArgumentGuardVariable& var) {
 		// result = prefix + "#" + var.decl.name;
@@ -217,7 +238,8 @@ struct TranslationVisitorWithPrefix : public ObserverVisitor {
 		handle_comparison(guard, "=");
 	}
 	void visit(const NeqGuard& guard) {
-		handle_comparison(guard, "!=");
+		handle_comparison(guard, "=");
+		result = "(not " + result + ")";
 	}
 	void visit(const ConjunctionGuard& guard) {
 		std::vector<std::string> conjuncts;
@@ -227,10 +249,11 @@ struct TranslationVisitorWithPrefix : public ObserverVisitor {
 			conjuncts.push_back(result);
 		}
 		result = "(and ";
+		assert(conjuncts.size() > 0);
 		for (const auto& conj : conjuncts) {
-			result = result + " " + conj;
+			result += " " + conj;
 		}
-		result = ")";
+		result += ")";
 	}
 
 	void visit(const State& /*obj*/) { throw std::logic_error("Unexpected invocation: TranslationVisitorWithPrefix::visit(const State&)"); }
@@ -239,72 +262,97 @@ struct TranslationVisitorWithPrefix : public ObserverVisitor {
 };
 
 
-// returns pair <decl, enc> containing the variable declarations and the actual encoding
-std::pair<std::string, std::string> make_formula(const Guard& guard, std::string param_prefix) {
+TranslationUnit make_formula(const Guard& guard, std::string param_prefix) {
 	TranslationVisitorWithPrefix visitor(param_prefix);
 	guard.accept(visitor);
 	return visitor.get_result();
 }
 
-// returns pair <"", formula>
-std::string make_relation_formula(const Function& label, const std::string& prefix, const std::string& otherPrefix, const SimulationEngine::VariableDeclarationSet& skip) {
-	std::string result;
+TranslationUnit make_relation_formula(const Function& label, const std::string& prefix, const std::string& otherPrefix, const SimulationEngine::VariableDeclarationSet& skip) {
+	TranslationUnit result;
 	for (const auto& decl : label.args) {
 		if (skip.count(*decl) == 0) {
+			// TODO: better way to compute names that is guaranteed to be consistent with TranslationVisitorWithPrefix
 			std::string var = prefix + decl->name;
 			std::string otherVar = otherPrefix + decl->name;
-			result = result + "(assert (= " + var + " " + otherVar + ")) ";
+			result.names.insert(var);
+			result.names.insert(otherVar);
+			result.assertions.push_back("(= " + var + " " + otherVar + ")");
 		}
 	}
 	return result;
 }
 
-std::string combine_formulas(std::vector<std::pair<std::string, std::string>> formulas) {
-	std::string result_decl;
-	std::string result_formula;
-	for (const auto& [decl, formula] : formulas) {
-		result_decl = result_decl + decl + " ";
-		result_formula = result_formula + formula + " ";
-	}
-	return result_decl + result_formula;
-}
-
 z3::check_result check_formula(std::string formula) {
 	z3::context context;
-	auto parse_result = context.parse_string(formula.c_str());
 	z3::solver solver(context);
+	solver.from_string(formula.c_str());
 	return solver.check();
 }
 
+bool is_transition_definitely_enabled(const TranslationUnit& relation, const TranslationUnit& match, const TranslationUnit& transition) {
+	// construct implication formula
+	auto implication = TranslationUnit::merge(match, transition);
+	implication.assertions.clear();
+	assert(match.assertions.size() == 1);
+	assert(transition.assertions.size() == 1);
+	std::string impl = "(not (=> " + match.assertions.at(0) + " " + transition.assertions.at(0) + "))";
+	implication.assertions = { impl };
+
+	// check for the implication being a tautology (iff the negation is unsat)
+	auto to_check = TranslationUnit::merge(relation, implication);
+	return check_formula(to_check.to_string()) == z3::unsat;
+}
+
 std::vector<const State*> abstract_post(const Observer& observer, const State& to_post, const Transition& match, const SimulationEngine::VariableDeclarationSet& unrelated={}) {
-	static const std::string PREFIX_MATCH = "::1::";
-	static const std::string PREFIX_TRANS = "::2::";
+	static const std::string PREFIX_MATCH = "arg1__";
+	static const std::string PREFIX_TRANS = "arg2__";
 
 	auto match_enc = make_formula(*match.guard, PREFIX_MATCH);
 	auto relation = make_relation_formula(match.label, PREFIX_MATCH, PREFIX_TRANS, unrelated);
 
 	std::vector<const State*> result;
+	bool definitely_has_post = false;
 	for (const auto& transition : observer.transitions) {
-		if (&transition->src == &to_post && &transition->label == &match.label) {
+		if (&transition->src == &to_post && &transition->label == &match.label && transition->kind == match.kind) {
 			auto trans_enc = make_formula(*transition->guard, PREFIX_TRANS);
-			auto formula = combine_formulas({{ match_enc, trans_enc, { "", relation } }});
+			auto formula = TranslationUnit::merge(relation, match_enc, trans_enc).to_string();
 
 			// TODO: use push/pop to avoid repeated construction of z3::context/z3::solver objects
+			// One could do the following:
+			//  - Create a context and solver per invocation of this function.
+			//  - add the decl/assertions from relation to the solver
+			//  - add the decl/assertions from match_enc to the solver
+			//  - push
+			//  - add the decl/assertions from trans_enc to the solver
+			//  - solve (check sat)
+			//  - pop
+			//  - push
+			//  - add the decl from trans_enc to the solver
+			//  - add the negated assertion from trans_enc to the solver
+			//  - solve (check unsat)
+			//  - pop
 			auto check_result = check_formula(formula);
 			switch (check_result) {
 				case z3::sat:
-				case z3::unknown:
 					// transition->guard may be enabled
 					result.push_back(&transition->dst);
+					definitely_has_post = definitely_has_post || is_transition_definitely_enabled(relation, match_enc, trans_enc);
 					break;
 				case z3::unsat:
 					// transition->guard is definitely not enabled
 					break;
+				case z3::unknown:
+					throw std::logic_error("SMT solving failed: Z3 was unable to prove SAT/UNSAT, returned 'UNKNOWN'. Cannot recover.");
 			}
 		}
 	}
 
-	result.push_back(&to_post); // TODO: implement a check and add 'state' conditionally // TODO: needed? the simulation relation should not be affected!?
+	if (!definitely_has_post) {
+		// only add the state back again if needed (unknown if there is an enabled outgoing transition)
+		result.push_back(&to_post);
+	}
+
 	return result;
 }
 
@@ -328,12 +376,30 @@ bool SimulationEngine::is_safe(const Enter& enter, const std::vector<std::refere
 }
 
 
-bool simulation_holds(const Observer& observer, SimulationEngine::SimulationRelation current, const State& to_simulate, const State& simulator) {
+bool simulation_holds(const Observer& observer, const SimulationEngine::SimulationRelation& current, const State& to_simulate, const State& simulator) {
+	if (&to_simulate == &simulator) {
+		auto required = std::make_pair(&to_simulate, &simulator);
+		return current.count(required) != 0;
+	}
+	// handle outgoing transitions of to_simulate
 	for (const auto& transition : observer.transitions) {
 		if (&transition->src == &to_simulate) {
 			auto next_to_simulate = &transition->dst;
 			auto simulator_post = abstract_post(observer, simulator, *transition);
 			for (const auto& next_simulator : simulator_post) {
+				auto required = std::make_pair(next_to_simulate, next_simulator);
+				if (current.count(required) == 0) {
+					return false;
+				}
+			}
+		}
+	}
+	// handle outgoing transitions of simulator
+	for (const auto& transition : observer.transitions) {
+		if (&transition->src == &simulator) {
+			auto next_simulator = &transition->dst;
+			auto to_simulate_post = abstract_post(observer, to_simulate, *transition);
+			for (const auto& next_to_simulate : to_simulate_post) {
 				auto required = std::make_pair(next_to_simulate, next_simulator);
 				if (!current.count(required)) {
 					return false;
@@ -343,6 +409,13 @@ bool simulation_holds(const Observer& observer, SimulationEngine::SimulationRela
 	}
 	return true;
 }
+
+// void debug_sim_rel(const SimulationEngine::SimulationRelation& sim) {
+// 	std::cout << "Simulation Relation: " << std::endl;
+// 	for (const auto& [lhs, rhs] : sim) {
+// 			std::cout << "  " << lhs->name << "  -  " << rhs->name << std::endl;
+// 	}
+// }
 
 void SimulationEngine::compute_simulation(const Observer& observer) {
 	SimulationEngine::SimulationRelation result;
@@ -374,6 +447,7 @@ void SimulationEngine::compute_simulation(const Observer& observer) {
 	// store information
 	this->observers.insert(&observer);
 	this->all_simulations.insert(result.begin(), result.end());
+	// debug_sim_rel(result);
 }
 
 bool SimulationEngine::is_in_simulation_relation(const State& state, const State& other) const {
